6dbfd0e191c92ad51ac09ddb40c043053fc4d0b5
Emerson Mendes <devmendesc@gmail.com>
2023-08-07 10:56:20 -0300

    alterações para executar o deploy

--- src/main/java/com/ibm/hospedagem/HospedagemApplication.java
+++ src/main/java/com/ibm/hospedagem/HospedagemApplication.java
@@ -1,10 +1,12 @@
 package com.ibm.hospedagem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 public class HospedagemApplication {
 
 	public static void main(String[] args) {
