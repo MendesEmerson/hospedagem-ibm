f4dca66abd0d31d7b0cdd0ac008327bbc5615f58
Emerson Mendes <devmendesc@gmail.com>
2023-08-21 10:54:33 -0300

    criação modelo e DTO de usuario

---
+++ src/main/java/com/ibm/hospedagem/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.ibm.hospedagem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 15)
+    private String password;
+
+    @OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Hospedagem> hospedagens = new ArrayList<Hospedagem>();
+
+}
